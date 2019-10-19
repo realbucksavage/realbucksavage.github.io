e8625e9782040f766c790dcafc1b9221807c7889
Jay <jgodara@sarvika.com>
2019-10-20 01:20:01 +0530

    Added the single fucking blog i've writted

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,6 +11,23 @@ export class Content extends Component {
           <Projects />
 
           {conf.interests.enabled && <Interests />}
+
+          <h2>My Thoughts</h2>
+          <p className="f4 mb-4 text-gray">Articles I've written.</p>
+          <div className="d-sm-flex flex-wrap gutter-condensed mb-4">
+            <div className="col-sm-6 col-md-12 col-lg-6 col-xl-4 mb-3">
+              <div className="height-full text-left border border-gray-light bg-white rounded-1 p-3">
+                <div className="d-flex flex-justify-between flex-items-start mb-1">
+                  <h1 className="f4 lh-condensed mb-1">
+                    <a href="/2019/03/09/nodemcu">
+                      Getting started with NodeMCU and MicroPython
+                    </a>
+                  </h1>
+                </div>
+                <div className="text-gray mb-2 ws-normal">Mar 09, 2019</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
